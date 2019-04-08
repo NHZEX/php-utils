ea6e67568f9ebc911a9663b96f4430748190fb9e
auooru <auooru@163.com>
2019-04-08 15:26:40 +0800

    add unit convertor

---
+++ tests/UnitConvertor/RenMinBiTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\UnitConvertor;
+
+use HZEX\UnitConvertor\RenMinBi;
+use Tests\Base;
+
+class RenMinBiTest extends Base
+{
+    public function fenToYuanProvider()
+    {
+        return [
+            [100, 1],
+            [57, 0.57],
+            [57, '0.57'],
+        ];
+    }
+
+    public function yuanToFenProvider()
+    {
+        return [
+            [57, 5700],
+            [0.57, 57],
+            [0.571, 57],
+            [0.57, '57'],
+        ];
+    }
+
+    /**
+     * @dataProvider fenToYuanProvider
+     * @param $yuan
+     * @param $fen
+     */
+    public function testFenToYuan($fen, $yuan)
+    {
+        $this->assertEquals($yuan, RenMinBi::fenToYuan($fen));
+    }
+
+    /**
+     * @dataProvider yuanToFenProvider
+     * @param $yuan
+     * @param $fen
+     */
+    public function testYuanToFen($yuan, $fen)
+    {
+        $this->assertEquals($fen, RenMinBi::yuanToFen($yuan));
+    }
+}
