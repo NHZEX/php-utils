e0a07b88b5bcbbe0f298b424c0262dfacc770a20
auooru <auooru@163.com>
2019-10-30 14:50:31 +0800

    添加功能

--- src/reflection_helper.php
+++ src/reflection_helper.php
@@ -6,6 +6,7 @@ use ReflectionException;
 use ReflectionObject;
 
 /**
+ * 反射获取类属性操作对象
  * @param object $object
  * @param string $prop
  * @return Property
@@ -16,4 +17,44 @@ function ref_get_prop($object, string $prop): Property
     $ref = new ReflectionObject($object);
     $refProp = $ref->getProperty($prop);
     return new Property($object, $refProp);
+}
+
+/**
+ * 引用传递类属性值
+ * @param $origin
+ * @param $target
+ * @param $prop
+ */
+function ref_prop_value($origin, $target, $prop)
+{
+    $originGet = function &($prop) {
+        return $this->{$prop};
+    };
+    $originGet = $originGet->bindTo($origin, $origin);
+
+    $targetSet = function ($prop) use ($originGet) {
+        $this->{$prop} = &$originGet($prop);
+    };
+    $targetSet = $targetSet->bindTo($target, $target);
+    $targetSet($prop);
+}
+
+/**
+ * 拷贝类属性值
+ * @param $origin
+ * @param $target
+ * @param $prop
+ */
+function ref_copy_prop_value($origin, $target, $prop)
+{
+    $originGet = function ($prop) {
+        return $this->{$prop};
+    };
+    $originGet = $originGet->bindTo($origin, $origin);
+
+    $targetSet = function ($prop) use ($originGet) {
+        $this->{$prop} = $originGet($prop);
+    };
+    $targetSet = $targetSet->bindTo($target, $target);
+    $targetSet($prop);
 }
