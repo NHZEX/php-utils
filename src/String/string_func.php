e0da56b6a853cf4eb088e843d34d4fcfb1d2b8ab
auooru <auooru@163.com>
2021-02-04 16:05:49 +0800

    update: 调整参数命名

--- src/String/string_func.php
+++ src/String/string_func.php
@@ -10,17 +10,17 @@ use function strlen;
 /**
  * 支持多字节字符串按照字节长度进行截取
  * @param string      $string  字符串
- * @param int         $length  截取长度
+ * @param int         $bytes   截取长度
  * @param string      $dot     省略符
  * @param string|null $charset 编码
  * @return string
  */
-function strcut_omit(string $string, int $length, string $dot = '...', ?string $charset = null): string
+function strcut_omit(string $string, int $bytes, string $dot = '...', ?string $charset = null): string
 {
     $dotlen = strlen($dot);
-    if (strlen($string) > $length - $dotlen) {
+    if (strlen($string) > $bytes - $dotlen) {
         $charset || $charset = mb_internal_encoding();
-        $cutlen = $length - $dotlen;
+        $cutlen = $bytes - $dotlen;
         // cutlen 最少保证不少于3字节
         return mb_strcut($string, 0, max($cutlen, 3), $charset) . $dot;
     }
