76fab8f38c92ef86972ef441242f20de58360067
auooru <auooru@outlook.com>
2022-08-08 15:40:33 +0800

    add: rector

---
+++ rector.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->skip([
+        __DIR__ . '/tests/DataStruct',
+    ]);
+
+    // register a single rule
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    // define sets of rules
+    $rectorConfig->sets([
+        // SetList::CODE_QUALITY,
+        LevelSetList::UP_TO_PHP_72
+    ]);
+
+    $rectorConfig->phpVersion(PhpVersion::PHP_72);
+};
